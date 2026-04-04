f7217236dba0058802111b9b62c0aa8d9204c0b9
starci183 <cuongnvtse160875@gmail.com>
2026-04-04 17:59:38 +0700

    Remove CORS origins logging from application bootstrap process

--- apps/core/src/main.ts
+++ apps/core/src/main.ts
@@ -42,7 +42,6 @@ const bootstrap = async () => {
     )
     // set the app to the globalThis object
     globalThis.__APP__ = app
-    console.log(envConfig().cors.origins)
     setupCors(app)
     setupSwagger({
         app,
