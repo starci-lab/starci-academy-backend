79325afd2de5de772657060f7ef72e6f9fb3a39c
starci183 <cuongnvtse160875@gmail.com>
2026-04-04 18:15:39 +0700

    Add newline at the end of primary.decorators.ts file

--- src/modules/databases/postgresql/primary/primary.decorators.ts
+++ src/modules/databases/postgresql/primary/primary.decorators.ts
@@ -4,5 +4,6 @@ import {
 import {
     POSTGRESQL_PRIMARY 
 } from "./constants"
+
 // InjectPostgresql function to inject the postgresql connection based on options
 export const InjectPrimaryPostgreSQLEntityManager = () => InjectEntityManager(POSTGRESQL_PRIMARY)
