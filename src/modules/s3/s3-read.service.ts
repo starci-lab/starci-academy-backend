3bf67dd36b1075400d3829c9383107a662eed36a
starci183 <cuongnvtse160875@gmail.com>
2026-04-25 17:20:31 +0700

    push

--- src/modules/s3/s3-read.service.ts
+++ src/modules/s3/s3-read.service.ts
@@ -68,14 +68,16 @@ export class S3ReadService {
         let s3Client: S3Client
         let bucket: string
         switch (provider) {
-        case S3Provider.DigitalOcean:
+        case S3Provider.DigitalOcean: {
             s3Client = this.s3
             bucket = envConfig().s3.digitalOcean.bucket
             break
-        case S3Provider.Minio:
+        }
+        case S3Provider.Minio: {
             s3Client = this.minioS3
             bucket = envConfig().s3.minio.bucket
             break
+        }
         }
         try {
             const result = await s3Client.send(
